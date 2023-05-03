ecbfd11b1f7f17680f9ec7ac68e3dc873599eb8c
Roman <brysommer1@gmail.com>
2023-05-03 14:51:56 +0300

    menu + lot number tied to customer and little bit 'govnocod'

--- anketa.js
+++ anketa.js
@@ -8,6 +8,8 @@ import { dataBot } from './values.js';
 let customerPhone;
 let customerName;
 let customerInfo = {};
+let selectedOrderRaw;
+
 
 
 const spreadsheetId = dataBot.googleSheetId;
@@ -40,54 +42,53 @@ const keyboards = {
 }
 
 export const anketaListiner = async() => {
-    let selectedOrderRaw;
+    bot.setMyCommands([
+      {command: '/start', description: 'Почати'},
+      {command: '/list', description: 'Показати доступні лоти'},
+    ]);
     bot.onText(/\/start/ , (msg) => {
         customerPhone = undefined;
         customerName = undefined;
         bot.sendMessage(msg.chat.id, phrases.greetings, {
-            reply_markup: {
-            keyboard: keyboards.startingKeyboard,
-            resize_keyboard: true,
-            one_time_keyboard: true
-            }
+            reply_markup: { keyboard: keyboards.startingKeyboard, resize_keyboard: true, one_time_keyboard: true }
         });
     });
     //'Купити ділянку' button handler
     bot.on("callback_query", async (query) => {
       selectedOrderRaw = query.data;
       const chatId = query.message.chat.id;
-      const range = `post!N${selectedOrderRaw}`;
+      customerInfo[chatId] = { lotNumber : query.data, phone: undefined, name: undefined };
+      const range = `post!N${ customerInfo[chatId].lotNumber }`;
       const statusNew = await searchForNew(spreadsheetId, range)
       if (statusNew) {
-        sendToRawStatusReserve(selectedOrderRaw);
-        bot.sendMessage(chatId, phrases.contactRequest, { reply_markup: { keyboard: keyboards.contactRequest, resize_keyboard: true } });
+        sendToRawStatusReserve(customerInfo[chatId].lotNumber);
+        bot.sendMessage(chatId, phrases.contactRequest, { reply_markup: { keyboard: keyboards.contactRequest, resize_keyboard: true }});
       } else bot.sendMessage(chatId, 'є замовлення від іншого користувача');
     })
+    bot.onText(/\/list/ , async (msg) => {
+      await sendNewRowsToTelegram(spreadsheetId, dataBot.googleSheetName, dataBot.lotStatusColumn, msg.chat.id, bot);     
+    });
     bot.on('message', async (msg) => {
       console.log(customerInfo);
       const chatId = msg.chat.id;
       if (msg.text === 'Зробити замовлення') await sendNewRowsToTelegram(spreadsheetId, dataBot.googleSheetName, dataBot.lotStatusColumn, chatId, bot);
       else if (msg.contact) {
-        customerInfo[chatId] = { name : msg.contact.first_name, phone : msg.contact.phone_number};
+        customerInfo[chatId].name = msg.contact.first_name;
+        customerInfo[chatId].phone = msg.contact.phone_number;
+        //customerInfo[chatId] = { name : msg.contact.first_name, phone : msg.contact.phone_number};
         customerPhone = msg.contact.phone_number;
         customerName = msg.contact.first_name;
-        bot.sendMessage(chatId, `Ваш номер телефону: ${msg.contact.phone_number}. Ваше імя ${msg.contact.first_name}. Дані вірні?`, 
-          {
-            reply_markup: {
-              keyboard: keyboards.dataConfirmation,
-              resize_keyboard: true,
-              one_time_keyboard: true
-            },
-          });
+        bot.sendMessage(chatId, `Ваш номер телефону: ${customerInfo[chatId].phone}. Ваше імя ${customerInfo[chatId].name}. Дані вірні?`, { 
+          reply_markup: { keyboard: keyboards.dataConfirmation, resize_keyboard: true, one_time_keyboard: true }});
       } else if(msg.text === 'Так, Оформити замовлення') {
           // переписати функції запису даних згідно рядка а не колонки
-          await sendToRawContact(customerInfo[chatId].phone, customerInfo[chatId].name, selectedOrderRaw);
-          await sendToRawStatusDone(selectedOrderRaw);
-          const range = `post!A${selectedOrderRaw}:E${selectedOrderRaw}`;
+          await sendToRawContact(customerInfo[chatId].phone, customerInfo[chatId].name, customerInfo[chatId].lotNumber);
+          await sendToRawStatusDone(customerInfo[chatId].lotNumber);
+          const range = `post!A${customerInfo[chatId].lotNumber}:E${customerInfo[chatId].lotNumber}`;
           const data = await getSpreadsheetData(spreadsheetId, range);
           if (data.values && data.values.length > 0) {
           const message = data.values[0].join('\n');
-          const idToDelete = await googleFindMessageId(selectedOrderRaw)
+          const idToDelete = await googleFindMessageId(customerInfo[chatId].lotNumber)
           await changeMessage(idToDelete, message);
           }
           bot.sendMessage(chatId, `Замовлення успішно оформлено. Дякую ${customerInfo[chatId].name}`);
@@ -100,7 +101,7 @@ export const anketaListiner = async() => {
           reply_markup: { keyboard: keyboards.enterPhone, resize_keyboard: true },
         });
       } else if (phoneRegex.test(msg.text)) {
-        customerInfo[chatId] = { phone : msg.text };
+        customerInfo[chatId].phone = msg.text;
         customerPhone = msg.text;
         bot.sendMessage(chatId, phrases.nameRequest);
       } else if (customerPhone && customerName == undefined ) {
